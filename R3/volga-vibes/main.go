9f06e88a3aef5746cc294ddba486baf75da89d2e
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-10-01 19:05:23 +0300

    feat(volga-vibes): listen via mTLS

--- R3/volga-vibes/main.go
+++ R3/volga-vibes/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
-	"net"
 	"os"
 
 	"github.com/haruulzangi/2023/R3/volga-vibes/core"
@@ -19,9 +20,27 @@ func main() {
 		port = "3000"
 	}
 
-	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%s", host, port))
+	caCertPool := x509.NewCertPool()
+	caCertFile, err := os.ReadFile("./certs/ca.crt")
 	if err != nil {
-		panic(err)
+		log.Panic("Failed to load CA cert file: ", err)
+	}
+	caCertPool.AppendCertsFromPEM(caCertFile)
+
+	cert, err := tls.LoadX509KeyPair("./certs/host.crt", "./certs/host.key")
+	if err != nil {
+		log.Panic("Failed to load host certificate or private key: ", err)
+	}
+
+	tlsConfig := &tls.Config{
+		ClientCAs:    caCertPool,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		MinVersion:   tls.VersionTLS12,
+		Certificates: []tls.Certificate{cert},
+	}
+	listener, err := tls.Listen("tcp", fmt.Sprintf("%s:%s", host, port), tlsConfig)
+	if err != nil {
+		log.Panic("Failed to listen: ", err)
 	}
 	log.Info("Server listening on ", listener.Addr().String())
 
