c8fc1da575ba9bb2f0cc32bd1e40e81d6eb5f84e
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-10-02 11:31:09 +0300

    refactor(checker): move certificates to the separate directory

--- R3/checkers/volga-vibes/scripts/gen-certs.sh
+++ R3/checkers/volga-vibes/scripts/gen-certs.sh
@@ -1,14 +1,14 @@
 set -e
 
-openssl ecparam -genkey -name secp384r1 -out ca.key
-openssl req -x509 -new -sha512 -nodes -key ca.key -days 1342 -out ca.crt -config conf/ca.conf
+openssl ecparam -genkey -name secp384r1 -out certs/ca.key
+openssl req -x509 -new -sha512 -nodes -key certs/ca.key -days 1342 -out certs/ca.crt -config conf/ca.conf
 
-openssl ecparam -genkey -name secp384r1 -out host.key
-openssl req -new -sha512 -nodes -key host.key -out host.csr -config conf/host.conf
-openssl x509 -req -sha512 -days 1342 -in host.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out host.crt -extfile conf/host-ext.conf
+openssl ecparam -genkey -name secp384r1 -out certs/host.key
+openssl req -new -sha512 -nodes -key certs/host.key -out certs/host.csr -config conf/host.conf
+openssl x509 -req -sha512 -days 1342 -in certs/host.csr -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out certs/host.crt -extfile conf/host-ext.conf
 
-openssl ecparam -genkey -name secp384r1 -out checker.key
-openssl req -new -sha512 -nodes -key checker.key -out checker.csr -config conf/checker.conf
-openssl x509 -req -sha512 -days 1342 -in checker.csr -CA ca.crt -CAkey ca.key -CAserial ca.srl -out checker.crt -extfile conf/checker-ext.conf
+openssl ecparam -genkey -name secp384r1 -out certs/checker.key
+openssl req -new -sha512 -nodes -key certs/checker.key -out certs/checker.csr -config conf/checker.conf
+openssl x509 -req -sha512 -days 1342 -in certs/checker.csr -CA certs/ca.crt -CAkey certs/ca.key -CAserial certs/ca.srl -out certs/checker.crt -extfile conf/checker-ext.conf
 
-rm host.csr checker.csr
+rm certs/*.csr
