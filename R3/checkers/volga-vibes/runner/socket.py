8305f7454b4676adc4556ec1b878fe08b1885f73
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-10-03 12:30:10 +0300

    feat(checker-socket): allow to use different roles

--- R3/checkers/volga-vibes/runner/socket.py
+++ R3/checkers/volga-vibes/runner/socket.py
@@ -7,14 +7,14 @@ class Socket:
     _socket: socket.socket
     connection: ssl.SSLSocket
 
-    def __init__(self, host: str, port: int):
+    def __init__(self, host: str, port: int, role = 'checker'):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((host, port))
 
         ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         ctx.verify_mode = ssl.CERT_REQUIRED
         ctx.load_verify_locations("certs/ca.crt")
-        ctx.load_cert_chain(certfile="certs/checker.crt", keyfile="certs/checker.key")
+        ctx.load_cert_chain(certfile=f"certs/{role}.crt", keyfile=f"certs/{role}.key")
 
         tls_sock = ctx.wrap_socket(sock, server_side=False)
         peer_cert = tls_sock.getpeercert()
